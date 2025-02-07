c7b6db54207a66dde89de0174dd9e9f8bd07d8c7
AnantSutar <ananthbksutar@gmail.com>
2025-02-08 00:02:58 +1100

    Repository pattern and some of get and read methods implemented

---
+++ PokemonReview/Helper/MappingProfiles.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using PokemonReview.DTO;
+using PokemonReview.Models;
+
+namespace PokemonReview.Helper
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Pokemon, PokemonDto>();
+        }
+    }
+}
